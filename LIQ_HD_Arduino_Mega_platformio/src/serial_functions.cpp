88888ed9a274eff2094e4bcb3e672694fc1bfab2
David (secondary Dell) <davidswygart@gmail.com>
2025-07-08 15:14:00 -0400

    refactor threshold setting via serial

--- LIQ_HD_Arduino_Mega_platformio/src/serial_functions.cpp
+++ LIQ_HD_Arduino_Mega_platformio/src/serial_functions.cpp
@@ -2,30 +2,54 @@
 #include "globals.h"
 #include "sensor_functions.h"
 
-void setThresholdsFromSerialInput() {
-	static String inputBuffer = "";
+static String inputBuffer = "";
+
+bool update_buffer() {
+	// updates string buffer
+	// returns true as soon as a newline character is encountered
 	while (Serial.available()) {
 		char c = Serial.read();
+		Serial.print(c);
 		if (c == '\n') {
-			inputBuffer.trim();
-			if (inputBuffer.length() > 0) {
-				int value = inputBuffer.substring(1).toInt();
-				if (inputBuffer[0] == 't'){
-					Serial.print("Set touch threshold to: ");
-					Serial.println(value);
-					touch_threshold = value;
-				}
-				else if(inputBuffer[0] == 'r'){
-					Serial.print("Set release threshold to: ");
-					Serial.println(value);
-					release_threshold = value;
-				}
-				set_sensor_settings();
-			}
-			inputBuffer = ""; // Clear buffer for next command
+			return true;
 		}
 		else {
 			inputBuffer += c;
 		}
 	}
+	return false;
 }
+
+bool update_thresh_global(){
+	inputBuffer.trim();
+	if (inputBuffer.length() < 2) {
+		Serial.println("Invalid entry: input too short");
+		return false;
+	}
+
+	int value = inputBuffer.substring(1).toInt();
+	if (inputBuffer[0] == 't'){
+		Serial.print("Set touch threshold to: "); Serial.println(value);
+		touch_threshold = value;
+		return true;
+	}
+	else if(inputBuffer[0] == 'r'){
+		Serial.print("Set release threshold to: "); Serial.println(value);
+		release_threshold = value;
+		return true;
+	}
+	else {
+		Serial.println("Invalid entry: Must start with 't' or 'r' to set touch or release threshold");
+		return false;
+	}
+}
+
+void setThresholdsFromSerialInput() {
+	bool line_complete = update_buffer();
+	if (line_complete) {
+		bool parsed = update_thresh_global();
+		if (parsed) {set_sensor_settings();}
+		inputBuffer = ""; // Clear buffer for next command
+	}
+}
+
