6885e06850df6146021da9da93b18d8700cf0d7e
David Swygart <davidswygart@gmail.com>
2025-07-18 14:13:38 -0400

    remove unused function declarations

--- LIQ_HD_Arduino_Mega_platformio/src/sensor_functions.h
+++ LIQ_HD_Arduino_Mega_platformio/src/sensor_functions.h
@@ -10,7 +10,5 @@ void record();
 void set_sensor_settings();
 void print_thresholds();
 void check_single_sensor(int sensor, unsigned long now, bool currently_licking[PADS_PER_SENSOR]);
-bool lick_started(bool is_touched, int sensor, int pad, unsigned long now);
-bool lick_stopped(bool is_touched, int sensor, int pad, unsigned long now);
 
 #endif // SENSOR_FUNCTIONS_H
